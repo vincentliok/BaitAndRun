67b5789d858322bccd985d3ebe46e07f4f347bd5
DESKTOP-JGQ5GG9\Vincent Liok <vl1042@nyu.edu>
2021-05-08 20:43:38 -0400

    pressure plates

---
+++ Assets/Scripts/RedPlateScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedPlateScript : MonoBehaviour
+{
+    // Adjust in editor
+    [SerializeField]
+    private GameObject door;
+
+    // Helper variables
+    private bool isPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy" && !isPressed)
+        {
+            transform.position += new Vector3(0.0f, -0.4f, 0.0f);
+            Destroy(door);
+            isPressed = true;
+        }
+    }
+}
